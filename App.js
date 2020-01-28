649f4a20cecaacd11a9e5d52a3ddc5ca3b24529e
muhammadsarim555 <muhammadsarim555@gmail.com>
2020-01-28 15:57:12 -0800

    Add splash screen

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 // FILES
 import Navigation from './src/navigations';
-import { store, persistor } from "./src/store";
+import {store, persistor} from './src/store';
+import SplashScreen from 'react-native-splash-screen';
 
 export default class HomeScreen extends React.Component {
+  componentDidMount() {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+
+    SplashScreen.show();
+  }
+
   render() {
     console.disableYellowBox = true;
     return (
-
       <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Navigation />
-      </PersistGate>
-    </Provider>
-
-    )
+        <PersistGate loading={null} persistor={persistor}>
+          <Navigation />
+        </PersistGate>
+      </Provider>
+    );
   }
 }
